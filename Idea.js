19141146de2e97ccd44acd7882ea46af88104275
Tanya Zhuge <tangnazhuge@gmail.com>
2021-09-15 19:07:49 -0700

    Add Idea class and update main and index

---
+++ Idea.js
@@ -0,0 +1,19 @@
+class Idea {
+  constructor(title, body) {
+    this.id = Date.now();
+    this.title = title;
+    this.body = body;
+    this.isStar = false;
+  }
+
+  saveToStorage() {
+
+  }
+  deleteFromStorage() {
+
+  }
+
+  updateIdea() {
+
+  }
+};
