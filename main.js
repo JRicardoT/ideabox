91784ff55b054735a3aca4a1472d97787a1758f5
jose Tovar <jose.tovar102397@gmail.com>
2021-09-18 14:08:59 -0600

    Refactor renderCard function innerHTML

--- main.js
+++ main.js
@@ -26,21 +26,22 @@ function renderCard(newCard) {
   // cardContainer.innerHTML = '';
   //   for (var i = 0; i < ideas.length; i++) {
       cardContainer.innerHTML += `
-      <article class="card">
+      <article class="card" id="${newCard.id}">
         <figure class="star-box">
           <img class="star-image" src="./assets/star.svg" alt="star">
-          <img class="delete-image"src="./assets/delete.svg" alt="delete-image">
+          <img class="delete-image" src="./assets/delete.svg" alt="delete-image">
         </figure>
           <div class="idea-container">
-            <h2 class="idea-title">${ideas[i].title}</h2>
-            <p class="idea">${ideas[i].body}</p>
+            <h2 class="idea-title">${newCard.title}</h2>
+            <p class="idea">${newCard.body}</p>
           </div>
         <figure class="comment-box">
           <img class="comment-image" src="./assets/comment.svg" alt="comment-image">
           <p class="comment">Comment</p>
         </figure>
       </article>`
-  }
+
+
 };
 
 function clearInput() {
